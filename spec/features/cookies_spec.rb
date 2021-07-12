86d53ed80ede607fbea6a08f875c4aa0b7a85092
Toby Privett <toby@snaplab.co.uk>
2021-07-12 11:07:19 +0100

    Allow users to update cookie settings after initial selection
    https://eaflood.atlassian.net/browse/RUBY-1491

--- spec/features/cookies_spec.rb
+++ spec/features/cookies_spec.rb
@@ -25,13 +25,23 @@ RSpec.feature "Cookies", type: :feature do
     expect(page.source).to have_text("'script','dataLayer','GA_ID'")
   end
 
-  scenario "User rejects analytics cookies" do
+  scenario "User rejects analytics cookies and toggles their selection" do
     visit "/"
     click_on "Reject analytics cookies"
     expect(page).to have_text("You’ve rejected analytics cookies")
+    expect(page.source).to have_text("'script','dataLayer','ga-disable-GA_ID'")
 
-    click_on "Hide this message"
+    click_on "change your cookie settings"
+    expect(page).to have_css("h1", text: "Cookie settings")
 
+    choose "Use cookies that measure my website use"
+    click_on "Save and continue"
+    expect(page.source).to have_text("'script','dataLayer','GA_ID'")
+    expect(page).to have_text("You’ve set your cookie preferences.")
+    expect(page).to have_link("Go back to the page you were looking at.", href: "/")
+
+    choose "Do not use cookies that measure my website use"
+    click_on "Save and continue"
     expect(page.source).to have_text("'script','dataLayer','ga-disable-GA_ID'")
   end
 end
